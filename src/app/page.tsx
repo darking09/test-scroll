618afebf19b139f63539d5e2a3ff6b35852f6a1c
Fernando Torres <fernandojoset@gmail.com>
2023-04-19 10:15:28 -0500

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 import styles from './page.module.css'
 import ImageAnimation from '@/components/ImageAnimation';
 
-import { useScrollPercentage } from '@/hooks/useScrollPercentage';
-
-interface Props {
-  scrollPercentage: number;
-}
-
-export default function Home(props : Props) {
-  const { scrollPercentage } = props;
-
+export default function Home() {
   return (
     <main className={styles.main}>
       <ImageAnimation />
